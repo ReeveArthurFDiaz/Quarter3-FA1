0a59a6198ad68e1692831124c94e9f9d83d0a9f8
ReeveArthurFDiaz <reevearthur.diaz@smc.pshs.edu.ph>
2025-01-27 21:49:43 +0800

    Delete Scripts/problemOne.js
    Updated script complete as of 1-27-2025, 9:50 pm

--- Scripts/problemOne.js
+++
@@ -1,8 +0,0 @@
-var nickname = "Reeve";
-var height = parseInt(64);
-var weight = parseInt(69);
-
-var heightft = parseInt(height / 12);
-var heightrem = parseInt(height % 12);
-
-alert(heightft + "\'" + heightrem + "\"");
